4ffa957445a401a20f795b61bda4efb1aea85b9c
Carissa Hluchan <carissahluchan@gmail.com>
2024-09-09 11:37:23 -0600

    Add: form controller

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,25 +4,61 @@ import { useState } from 'react';
 
 function Form({ addTricks }) {
 
-    const [] = useState()
+    const [stance, setStance] = useState('');
+    const [name, setName] = useState('');
+    const [obstacle, setObstacle] = useState('');
+    const [tutorial, setTutorial] = useState('');
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newTrick = { stance, name, obstacle, tutorial }
+        addTricks(newTrick);
+        setStance('');
+        setName('');
+        setObstacle('');
+        setTutorial('');
+    }
 
     return (
-        <form>
-            <select>
-                <option value='choose your stance'>Choose your Stance</option>
+        <form onSubmit={handleSubmit}>
+            <select
+                className='stance'
+                value={stance}
+                onChange={(event => { setStance(event.target.value) })}
+            >
+                <option value='' disabled>Choose your Stance</option>
                 <option value='regular'>Regular</option>
                 <option value='switch'>Switch</option>
             </select>
-            <imput>Name of Trick</imput>
-            <select>
-                <option value='choose your stance'>Choose your Obstacle</option>
+
+            <input
+                className='name'
+                type='text'
+                placeholder='Name of Trick'
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+            />
+
+            <select
+                classname='obstacle'
+                value={obstacle}
+                onChange={(event => { setObstacle(event.target.value) })}
+            >
+                <option value='' disabled>Choose your Obstacle</option>
                 <option value='flat ground'>Flat Ground</option>
                 <option value='stairs'>Stairs</option>
                 <option value='ledge'>Ledge</option>
             </select>
-            <input>Link to Tutorial</input>
-            <button>Send it!</button>
+
+            <input
+                className='tutorial'
+                type='text'
+                placeholder='Link to Tutorial'
+                value={tutorial}
+                onChange={(event) => setTutorial(event.target.value)}
+            />
+
+            <button type='submit'>Send it!</button>
         </form>
     )
 }
